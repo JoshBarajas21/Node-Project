24ac23272ea2824b73fd51d4af6e4632527c15ee
Joshua Barajas <124693455+JoshBarajas21@users.noreply.github.com>
2023-04-21 16:12:02 +0000

    Creando Servidor 😎💥

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Hello World');
+//1. Importanto el modulo de Node
+//http
+import http from 'http';
+
+//2. Crear el servidor
+//la siguiente constante es el carro listo
+const server = http.createServer( ( req, res )=> {
+    //Toda la lógica del server
+    // 1. Respondiendo al cliente (res)
+    res.write("Hello from the server ...");
+    //2. Cerrar la conexión
+    res.end();
+} ); //const porque no va a cambiar el valor
+
+//3. Arrancando el servidor
+/* server.listen( <puerto>, <IP>, <callback>) */
+server.listen( 3000, "0.0.0.0", ()=>{
+    console.log(" 💘 Servidor Escuchando en http://localhost:3000")
+});
